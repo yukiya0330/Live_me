45a2da40ec595a1cdb17a5c18e074d10c8079433
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-27 03:51:38 +0000

    [Add]ログイン後遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin then
+      admin_path
+    when Customer then
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin then
+      new_admin_session_path
+    when :customer then
+      root_path
+    end
+  end
 end
