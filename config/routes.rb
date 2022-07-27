d0ef3a6c37392c3fb10f0bf026dc3f882dbef810
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-27 04:50:10 +0000

    [Add]rails g comtroller customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
