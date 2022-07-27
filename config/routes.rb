e6c836fe97668c7b746b571c152e5c6c04c1de53
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-27 03:28:04 +0000

    [Add]Devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
