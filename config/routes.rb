604f9053001edcf7366886a34ec9774ea14473ed
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-27 08:17:46 +0000

    [Add]編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -11,6 +14,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/' => 'homes#top'
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, except: [:show]
   end
   
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
