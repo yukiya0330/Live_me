1bbb2bc8e4ede967be1ba4f7adec48f7656576cf
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-29 00:51:29 +0000

    [Add]report

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'reports/index'
+    get 'reports/show'
+    get 'reports/new'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -26,6 +31,7 @@ Rails.application.routes.draw do
     resources :artists, only: [:show, :index]
     resources :lives, only: [:index, :show, :create]
     resources :live_houses, only: [:index, :show]
+    resources :reports
   end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
