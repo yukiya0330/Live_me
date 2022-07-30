2cbc7c29574a070c37658d31bf14597ff8157634
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-30 07:24:18 +0000

    [Add]favorite_genre

--- app/controllers/public/favorite_genres_controller.rb
+++ app/controllers/public/favorite_genres_controller.rb
@@ -1,2 +1,15 @@
 class Public::FavoriteGenresController < ApplicationController
+  def create
+    genre = Genre.find(params[:genre_id])
+    favorite_genre = current_customer.favorite_genres.new(genre_id: genre.id)
+    favorite_genre.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    genre = Genre.find(params[:genre_id])
+    favorite_genre = current_customer.favorite_genres.find_by(genre_id: genre.id)
+    favorite_genre.destroy
+    redirect_to request.referer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,9 @@ Rails.application.routes.draw do
     end
     resources :live_houses, only: [:index, :show]
     resources :reports
+    resources :genres, only: [:index] do
+      resource :favorite_genres, only: [:create, :destroy]
+    end  
   end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
