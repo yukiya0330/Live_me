b37b950f1b9a2a16d698fbb9b8519c5f4dd1fc3b
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-07-31 09:49:05 +0000

    [Add]ジャンルお気に入り機能

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,8 @@ class Genre < ApplicationRecord
   has_many :favorite_genres, dependent: :destroy
   has_many :artist_genres, dependent: :destroy
   has_many :artists, through: :artist_genres
+  
+  def favorited_by?(customer)
+    favorite_genres.where(customer_id: customer.id).exists?
+  end
 end
