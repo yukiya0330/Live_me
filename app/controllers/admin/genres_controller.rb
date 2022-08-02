4368a1d20859c0cc58b7d4b466a4c81c3b7bc211
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-08-02 02:36:10 +0000

    [Add]削除追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -26,6 +26,12 @@ class Admin::GenresController < ApplicationController
     end
   end
   
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to admin_genres_path
+  end
+  
   private
   def genre_params
     params.require(:genre).permit(:name)
