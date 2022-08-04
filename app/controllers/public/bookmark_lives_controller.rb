0a12d3bce14b862f5f0f977d634195b4b7e24f80
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-08-04 02:02:43 +0000

    [Update]ajax化

--- app/controllers/public/bookmark_lives_controller.rb
+++ app/controllers/public/bookmark_lives_controller.rb
@@ -1,15 +1,13 @@
 class Public::BookmarkLivesController < ApplicationController
   def create
-    live = Live.find(params[:live_id])
-    bookmark_live = current_customer.bookmark_lives.new(live_id: live.id)
+    @live = Live.find(params[:live_id])
+    bookmark_live = current_customer.bookmark_lives.new(live_id: @live.id)
     bookmark_live.save
-    redirect_to request.referer
   end
   
   def destroy
-    live = Live.find(params[:live_id])
-    bookmark_live = current_customer.bookmark_lives.find_by(live_id: live.id)
+    @live = Live.find(params[:live_id])
+    bookmark_live = current_customer.bookmark_lives.find_by(live_id: @live.id)
     bookmark_live.destroy
-    redirect_to request.referer
   end
 end
