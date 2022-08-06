fe99a3bf94db7e2edba75eef32d75778b0310e35
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-08-06 01:09:55 +0000

    [Edit]レイアウト編集

--- app/controllers/admin/artist_genres_controller.rb
+++ app/controllers/admin/artist_genres_controller.rb
@@ -8,6 +8,7 @@ class Admin::ArtistGenresController < ApplicationController
   def create
     @artist_genre = ArtistGenre.new(artist_genre_params)
     if @artist_genre.save
+      flash[:success] = "ジャンルが追加されました"
       @artist = @artist_genre.artist
       redirect_to request.referer
     else
@@ -19,6 +20,7 @@ class Admin::ArtistGenresController < ApplicationController
     @genre = Genre.find(params[:genre_id])
     @artist_genre = ArtistGenre.find_by(genre_id: @genre.id)
     @artist_genre.destroy
+    flash[:success] = "一件のジャンルが削除されました"
     redirect_to request.referer
   end
   
