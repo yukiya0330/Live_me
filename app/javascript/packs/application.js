9edeec1c3ca4c46a4e55bf8a9b5f72f499d876bd
yukiya0330 <97089962+yukiya0330@users.noreply.github.com>
2022-08-10 08:04:44 +0000

    [Update]ハンバーガーメニュー作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,16 +21,3 @@ ActiveStorage.start()
 $(function(){
   $('.alert').fadeOut(4000);  //４秒かけて消えていく
 });
-
-window.onload = function () {
-    var nav = document.getElementById('nav-wrapper');
-    var hamburger = document.getElementById('js-hamburger');
-    var blackBg = document.getElementById('js-black-bg');
-
-    hamburger.addEventListener('click', function () {
-        nav.classList.toggle('open');
-    });
-    blackBg.addEventListener('click', function () {
-        nav.classList.remove('open');
-    });
-};
